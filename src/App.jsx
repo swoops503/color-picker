af80f8097427677c20b4fc402c479b81b997b69d
swoops503 <james.roundy4@gmail.com>
2023-06-18 18:46:37 -0700

    first commit, added color function

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// Write your Color component here
+
+const Color = (props) => {
+  return <div className={props.color}></div>
+}
+
+const App = () => {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className="red">red</div>
+      </div>
+      <div id="colors-list">
+         <Color color="red"/>
+         <Color color="blue"/>
+  <Color color="green"/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
